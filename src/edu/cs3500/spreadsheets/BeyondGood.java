90c987e16780b849e415cd2636b922073370e3d7
gdandi04 <gdandi04@gmail.com>
2019-10-22 13:14:52 -0400

    change system out print

--- src/edu/cs3500/spreadsheets/BeyondGood.java
+++ src/edu/cs3500/spreadsheets/BeyondGood.java
@@ -15,6 +15,6 @@ public class BeyondGood {
       - evaluate all the cells, and
       - report any errors, or print the evaluated value of the requested cell.
     */
-    System.out.println("hellooo");
+    System.out.println("goodbye");
   }
 }
