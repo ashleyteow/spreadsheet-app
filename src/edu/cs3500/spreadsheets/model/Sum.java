ce801956f54587d3f18b2e1248c84d49b7e55aab
gdandi04 <gdandi04@gmail.com>
2019-10-26 23:26:02 -0400

    added functionality to Sum class and added tests for both Multiply and Sum

--- src/edu/cs3500/spreadsheets/model/Sum.java
+++ src/edu/cs3500/spreadsheets/model/Sum.java
@@ -1,12 +1,59 @@
 package edu.cs3500.spreadsheets.model;
 
 import edu.cs3500.spreadsheets.sexp.Sexp;
+import edu.cs3500.spreadsheets.sexp.SexpVisitor;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Sum implements Operation {
+/**
+ * Performs the sum operation for a formula.
+ */
+public class Sum implements Operation, SexpVisitor<Double> {
+
+  private double sum;
+  private ArrayList<Cell> cells;
+
+  public Sum(ArrayList<Cell> cells) {
+    this.cells = cells;
+    this.sum = 0;
+  }
 
   @Override
   public void operate() {
-    return;
+    for (Cell c : cells) {
+      c.getCellContents().getContents().accept(this);
+    }
   }
 
+  public double getSum() {
+    return this.sum;
+  }
+
+  @Override
+  public Double visitBoolean(boolean b) {
+    return sum;
+  }
+
+  @Override
+  public Double visitNumber(double d) {
+    return sum += d;
+  }
+
+  @Override
+  public Double visitSList(List<Sexp> l) {
+    for (Sexp s : l) {
+      s.accept(this);
+    }
+    return sum;
+  }
+
+  @Override
+  public Double visitSymbol(String s) {
+    return sum;
+  }
+
+  @Override
+  public Double visitString(String s) {
+    return sum;
+  }
 }
--- test/SumTest.java
+++ test/SumTest.java
@@ -3,42 +3,40 @@ import static org.junit.Assert.*;
 import edu.cs3500.spreadsheets.model.Cell;
 import edu.cs3500.spreadsheets.model.Coord;
 import edu.cs3500.spreadsheets.model.Multiply;
-import edu.cs3500.spreadsheets.model.Operation;
+import edu.cs3500.spreadsheets.model.Sum;
 import java.util.ArrayList;
 import org.junit.Test;
 
-public class MultiplyTest {
-
+public class SumTest {
   @Test
-  public void testProductWithOnlyNumbers() {
+  public void testSumWithOnlyNumbers() {
     ArrayList<Cell> cells = new ArrayList<Cell>();
     cells.add(new Cell(new Coord(1,1), "87"));
     cells.add(new Cell(new Coord(2,1), "3"));
-    Multiply prod = new Multiply(cells);
-    prod.operate();
-    assertEquals(261.0, prod.getProduct(), 0.0001);
+    Sum sum = new Sum(cells);
+    sum.operate();
+    assertEquals(90.0, sum.getSum(), 0.0001);
   }
 
   @Test
-  public void testProductWithNumbersAndStrings() {
+  public void testSumWithNumbersAndStrings() {
     ArrayList<Cell> cells = new ArrayList<Cell>();
     cells.add(new Cell(new Coord(1,1), "hello"));
     cells.add(new Cell(new Coord(2,1), "3"));
     cells.add(new Cell(new Coord(2,1), "cake"));
     cells.add(new Cell(new Coord(4, 2), "6"));
-    Multiply prod = new Multiply(cells);
-    prod.operate();
-    assertEquals(18.0, prod.getProduct(), 0.0001);
+    Sum sum = new Sum(cells);
+    sum.operate();
+    assertEquals(9.0, sum.getSum(), 0.0001);
   }
 
   @Test (expected = IllegalArgumentException.class)
-  public void testProductWithInvalidSexp() {
+  public void testSumWithInvalidSexp() {
     ArrayList<Cell> cells = new ArrayList<>();
     cells.add(new Cell(new Coord(2, 4), "\"|hello"));
     cells.add(new Cell(new Coord(4, 5), "5"));
     cells.add(new Cell(new Coord(5, 6), "5"));
-    Multiply prod = new Multiply(cells);
-    prod.operate();
+    Sum sum = new Sum(cells);
+    sum.operate();
   }
-
 }
