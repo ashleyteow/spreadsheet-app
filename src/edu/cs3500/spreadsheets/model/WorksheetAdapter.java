a2961b6ad89983fe5ddd6aaeded0377a515df6df
Ashley Teow <ashley.sw.teow@gmail.com>
2019-12-02 10:22:04 -0800

    add unimplemented evaluate method

---
+++ src/edu/cs3500/spreadsheets/model/WorksheetAdapter.java
@@ -0,0 +1,35 @@
+package edu.cs3500.spreadsheets.model;
+
+import edu.cs3500.spreadsheets.provider.model.IFormula;
+import edu.cs3500.spreadsheets.provider.view.SpreadSheetViewModel;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WorksheetAdapter implements SpreadSheetViewModel {
+  private Worksheet originalWorksheet;
+
+  @Override
+  public HashMap<Coord, IFormula> getSheet() {
+    return null;
+  }
+
+  @Override
+  public Coord getMaxCoord() {
+    return null;
+  }
+
+  @Override
+  public String retrieve(int col, int row) {
+    return null;
+  }
+
+  @Override
+  public Map<Coord, String> getAllRawContents() {
+    return null;
+  }
+
+  @Override
+  public String evaluate(int col, int row) {
+    return null;
+  }
+}
